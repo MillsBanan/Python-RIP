0bcd4378272567ad34532ab1e3b569236b1bc25b
Ryan <rrm36@uclive.ac.nz>
2020-05-06 14:17:23 +1200

    Fixed error handling so it actually works

--- rip_router.py
+++ rip_router.py
@@ -1,6 +1,8 @@
 import sys
 import re
+from time import time
 import socket
+import traceback
 import select
 
 """
@@ -16,7 +18,7 @@ TODO:
 
 class ConfigSyntaxError(Exception):
     """
-    A class to raise custom errors
+    A class to raise errors related to config file syntax
     """
 
     def __init__(self, message):
@@ -46,18 +48,19 @@ class ConfigData:
     ----------
     router_id : int
         Unique ID for the router in the network
-    intput_ports : list
+    input_ports : list
         Contains all of the port numbers (unique) that represent the interfaces
         of the router that are connected to adjacent routers
     outputs : list
-        Contains elements of form PortNum-Metric-ID for all adjacent routers.
-        PortNum represents the interface of adjacent routers that are directly
-        connected to the router.
+        Contains elements of form PortNum-Metric-ID for each router 'R' adjacent to router 'A'.
+        PortNum represents the interface that router R is connected to router A over.
+        Metric is the cost of the link from A to R.
+        ID is the router ID of router R.
 
     Methods
     -------
     read_config_file
-        Opens and stores the contents of a config file given as a
+        Opens and stores the contents of the config file given as a
         parameter on the command line.
     parse
 
@@ -69,11 +72,9 @@ class ConfigData:
 
     def __init__(self):
         file_data = self.read_config_file()
-
         self.router_id = None
         self.input_ports = None
         self.outputs = None
-
         self.parse(file_data)
 
     def read_config_file(self):
@@ -106,11 +107,9 @@ class ConfigData:
                     self.outputs = line[1:-1]
                 else:
                     raise ConfigSyntaxError("ConfigSyntaxError: Config file syntax is incorrect")
-
         except ConfigSyntaxError as error:
             print(str(error))
             sys.exit(1)
-
         self.parse_router_id()
         self.parse_input_ports()
         self.parse_outputs()
@@ -119,17 +118,17 @@ class ConfigData:
         try:
             self.router_id = int(self.router_id)
             if self.router_id < 1 or self.router_id > 64000:
-                raise ConfigSyntaxError(
-                    "ConfigSyntaxError: Router ID must be between 1 and 64000 inclusive!")
+                raise ConfigSyntaxError("Router ID must be between 1 and 64000 inclusive!")
         except ValueError:
-            print("ValueError: Router Id must be an integer")
+            traceback.print_exc()
+            print("ConfigSyntaxError: Router Id must be an integer")
             sys.exit(1)
         except ConfigSyntaxError as err:
-            print(str(err))
+            traceback.print_exc()
+            print(err)
             sys.exit(1)
 
     def parse_input_ports(self):
-
         for i in range(len(self.input_ports)):
             try:
                 self.input_ports[i] = int(self.input_ports[i])
@@ -147,7 +146,6 @@ class ConfigData:
 
     def parse_outputs(self):
         """I hate this method but can't make it less gross"""
-
         for i in range(len(self.outputs)):
             try:
                 router = [int(x) for x in self.outputs[i].split('-')]
@@ -180,12 +178,15 @@ class ConfigData:
             print(str(err))
             sys.exit(1)
 
-    def getneighbourrouter(self, router_ID):  # find neighbour by ID
-        for neighbour in self.outputs:
-            if neighbour['ID'] == router_ID:
-                return neighbour
-        else:
-            raise ConfigSyntaxError("ConfigSyntaxError: Neighbouring router not found")
+
+class ForwardingEntry:
+    """Class to hold information on forwarding table entries"""
+
+    def __init__(self, next_hop, port, metric):
+        self.next_hop_id = next_hop
+        self.next_hop_port = port
+        self.metric = metric
+        self.timer = time()
 
 
 class RipRouter:
@@ -193,47 +194,62 @@ class RipRouter:
 
     def __init__(self, router_config):
         self.router_config = router_config
-        self.inputsocket = []
-        self.outputsocket = None
+        self.input_sockets = []
+        self.output_socket = None
         self.address = '127.0.0.1'
-        self.forwardingtable = []
-
+        self.forwarding_table = dict()
         self.start()
 
     def start(self):
-        """Binds input sockets and sets the ouput socket to the first input socket (if it exists)"""
+        """Binds input sockets and sets the output socket to the first input socket (if it exists)"""
         for input_port in self.router_config.input_ports:
             try:
                 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                sock.bind(self.address, input_port)
-                self.inputsocket.append(sock)
-            except RouterError as err:
+                sock.bind((self.address, input_port))
+                self.input_sockets.append(sock)
+            except OSError as err:
                 print(str(err))
                 sys.exit(1)
-
-        if len(self.inputsocket) == 0:
-            raise ConfigSyntaxError("RouterError: Router has no attached inputs!")
-            # bruh not sure if this is how you want my errors
+        try:
+            if len(self.input_sockets) == 0:
+                raise RouterError("RouterError: Router has no attached inputs!")
+        except RouterError as err:
+            print(str(err))
             sys.exit(1)
         else:
-            self.outputsocket = self.inputsocket[0]
+            self.output_socket = self.input_sockets[0]
+            for neighbour in self.router_config.outputs:
+                entry = ForwardingEntry(neighbour["ID"], neighbour["Port"], neighbour["Metric"])
+                self.add_forwarding_entry(entry.next_hop_id, entry)
 
-    def send(self, data, destination):
-        """Send data to a neighbouring destination by router ID"""
-        neighbour = self.router_config.getneighbourrouter(destination)
-        self.inputsocket[0].sendto(data, (self.address, neighbour['Port']))
+    def send(self, data, router_id):
+        """Send data to a neighbouring router by router ID"""
+        try:
+            self.output_socket.sendto(data, (self.address, self.forwarding_table[router_id].next_hop_id))
+        except KeyError as err:
+            print("KeyError: Router {} is not in the forwarding table".format(err))
 
-    def addforwardingentry(self, id, nexthop, distance):
-        self.forwardingtable.append([id, nexthop, distance])
+    def add_forwarding_entry(self, router_id, entry):
+        """Adds an entry to the forwarding table"""
+        self.forwarding_table[router_id] = entry
 
-    def removeforwardingentry(self, id):
-        # Removes entries in forwarding table
-        for entry in self.forwardingtable:
-            if entry[0] == id:
-                self.forwardingtable.remove(entry)
+    def remove_forwarding_entry(self, router_id):
+        """Removes an entry from the forwarding table"""
+        try:
+            del self.forwarding_table[router_id]
+        except KeyError as err:
+            print("KeyError: Router {} is not in the forwarding table".format(err))
+
+    # Can probs just look in the forwarding table
+    # def get_neighbour_router(self, router_id):  # find neighbour by ID
+    #     for neighbour in self.router_config.outputs:
+    #         if neighbour['ID'] == router_id:
+    #             return neighbour
+    #     else:
+    #         raise ConfigSyntaxError("ConfigSyntaxError: Neighbouring router not found")
 
 
-class RIPdaemon:
+class RipDaemon:
     """RIP routing daemon which contains a router class which it controls"""
 
     def __init__(self, router):
@@ -245,13 +261,12 @@ class RIPdaemon:
 
 
 def main():
-
     router_config = ConfigData()
     print(router_config.router_id)
     print(router_config.input_ports)
     print(router_config.outputs)
     router = RipRouter(router_config)
-    RIPdaemon(router).start
+    RipDaemon(router).start
 
 
 if __name__ == "__main__":
